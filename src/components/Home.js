3990aac20daefd2e9724189d3bd3400ffa4063d2
Serhat Aksakal <hi@serhat.codes>
2021-08-10 04:50:05 +0300

    👌 IMPROVE: Updated home page to include actions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { addToCart } from './actions/cartActions'
 class Home extends Component {
+  handleClick = (id) => {
+    this.props.addToCart(id)
+  }
   render() {
     let itemList = this.props.items.map((item) => {
       return (
@@ -11,6 +15,9 @@ class Home extends Component {
             <span
               to="/"
               className="btn-floating halfway-fab waves-effect waves-light red"
+              onClick={() => {
+                this.handleClick(item.id)
+              }}
             >
               <i className="material-icons">add</i>
             </span>
@@ -37,4 +44,11 @@ const mapStateToProps = (state) => {
     items: state.items,
   }
 }
-export default connect(mapStateToProps)(Home)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (id) => {
+      dispatch(addToCart(id))
+    },
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
